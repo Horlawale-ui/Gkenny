a00009ee1eba84f54b4f6240ff2548a2b2c72048
Horlawale-ui <53876892+Horlawale-ui@users.noreply.github.com>
2022-04-12 15:08:27 +0100

    Upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AngularFireModule } from '@angular/fire/compat';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { environment } from 'src/environments/environment';
+import { AuthModule } from './layouts/auth/auth.module';
+import { DefaultModule } from './layouts/default/default.module';
+import { DashboardModule } from './layouts/dashboard/dashboard.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    DefaultModule,
+    AuthModule,
+    DashboardModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig)
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
